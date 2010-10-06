28f905ed1d3080978cbfbdbb5dae87b1ed8f88ba
Glen D Sanford <glen@twitter.com>
2010-10-06 15:23:56 -0700

    move to std proj 0.7.7

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val twitterNestRepository  = "twitter.com" at "http://www.lag.net/nest/"
   val scalaToolsReleases = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.0"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.7"
 }
