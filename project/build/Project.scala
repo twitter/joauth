b5be1d705c6d885e4c2b79edf6ac7a3517932f6a
Glen D Sanford <glen@twitter.com>
2010-11-10 14:53:20 -0800

    move joauth to thrust 1.0.1

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,10 +2,9 @@ import sbt._
 import com.twitter.sbt._
 
 class JoauthProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val vscaladoc = "org.scala-tools" % "vscaladoc" % "1.1-md-3"
   val servletapi = "javax.servlet" % "servlet-api" % "2.5"
   val codec = "commons-codec" % "commons-codec" % "1.4"
-  val thrust = "com.twitter" %% "thrust-core" % "1.0"
+  val thrust = "com.twitter" % "thrust-core_2.8.1" % "1.0.1"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
   val mockitoall = "org.mockito" % "mockito-all" % "1.8.4" % "test"
 
--- src/test/scala/com/twitter/joauth/testhelpers/MockRequest.scala
+++ src/test/scala/com/twitter/joauth/testhelpers/MockRequest.scala
@@ -1,10 +1,10 @@
 // Copyright 2010 Twitter, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
 // file except in compliance with the License. You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software distributed
 // under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
@@ -62,7 +62,7 @@ class MockRequest(
   val params: Params = null
   def remoteHost: String = remoteAddr
   val authType: String = null
-  val cookies: Array[Cookie] = null
+  val cookies: Seq[Cookie] = null
   val remoteUser: String = null
   val requestedSessionId: String = null
   def requestUri: String = path.toString
