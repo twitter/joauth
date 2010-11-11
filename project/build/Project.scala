51e866faa7c17b58c4b89279bd94076e1f46ea66
Glen D Sanford <glen@twitter.com>
2010-11-11 12:17:56 -0800

    move to thrust 1.0.2

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,7 +4,7 @@ import com.twitter.sbt._
 class JoauthProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
   val servletapi = "javax.servlet" % "servlet-api" % "2.5"
   val codec = "commons-codec" % "commons-codec" % "1.4"
-  val thrust = "com.twitter" % "thrust-core" % "1.0.1"
+  val thrust = "com.twitter" % "thrust-core" % "1.0.2"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val mockitoall = "org.mockito" % "mockito-all" % "1.8.4" % "test"
 
