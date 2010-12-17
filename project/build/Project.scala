4abbf56b2f5176af0e851913ab20895c06e7f163
marius a. eriksen <marius@twitter.com>
2010-12-17 11:21:09 -0800

    make joauth safe for inlining, bump version accordingly.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,13 +1,18 @@
 import sbt._
 import com.twitter.sbt._
 
-class JoauthProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+class JoauthProject(info: ProjectInfo)
+  extends StandardProject(info)
+  with SubversionPublisher
+  with InlineDependencies
+{
   override def managedStyle = ManagedStyle.Maven
   override def disableCrossPaths = true
 
+  inline("com.twitter" % "thrust" % "1.3.1")
+
   val servletapi = "javax.servlet" % "servlet-api" % "2.5"
   val codec = "commons-codec" % "commons-codec" % "1.4"
-  val thrust = "com.twitter" % "thrust" % "1.2.0"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val mockitoall = "org.mockito" % "mockito-all" % "1.8.4" % "test"
 
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
   val scalaToolsReleases = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
   val twitterMavenRepo = "maven.twttr.com" at "http://maven.twttr.com/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.11"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.18"
 }
