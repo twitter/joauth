4f1c7fdd164bf3a618c853a26d9eb447126baeaf
Glen D Sanford <glen@twitter.com>
2010-12-30 13:45:02 -0800

    remove unneeded servlet-api dependency

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -8,7 +8,6 @@ class JoauthProject(info: ProjectInfo)
   override def managedStyle = ManagedStyle.Maven
   override def disableCrossPaths = true
 
-  val servletapi = "javax.servlet" % "servlet-api" % "2.5"
   val codec = "commons-codec" % "commons-codec" % "1.4"
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test"
   val mockitoall = "org.mockito" % "mockito-all" % "1.8.4" % "test"
